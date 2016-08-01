ebc2ce02912393f8e4f597b73ffd62949b131973
= <faraon5593@gmail.com>
2016-08-01 20:11:31 +0200

    settings

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,13 +1,42 @@
-<template>
-  <div class="jumbotron">
-    <h2>Login</h2>
-    <p>Zaloguj się</strong>
-    </p>  
-  </div>
-</template>
+ <template>
+    <div class="col-sm-6 col-sm-offset-3">
+      <input type="text" v-model="username">
+      <input type="password" v-model="password">
+      <button class="btn btn-primary" v-on:click="getToken()">Zaloguj</button>
+      <div class="quote-area">
+        {{ response | json }}
+      </div>
+    </div>
+  </template>
 
-<script>
-</script>
+  <script>
+  import config from './../config'
 
-<style>
-</style>
+  export default {
+    data () {
+      return {
+        response: '',
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      getToken () {
+        var credentials =
+          {
+            'client_id': config.client_id,
+            'client_secret': config.client_secret,
+            'grant_type': 'password',
+            'username': this.username,
+            'password': this.password
+          }
+        this.$http
+          .post(config.apiEndpoint + '/oauth/v2/token', credentials).then(function (response) {
+            this.response = response.body
+          }).catch(function (error) {
+            console.log(error)
+          })
+      }
+    }
+  }
+  </script>
