ebb33d3fde1f8184c32da85326bb53e8a6b605f3
Kamil Łysek <Kamil ysek>
2016-08-11 20:40:29 +0200

    CRUD Buildings

--- api/src/AppBundle/Controller/BuildingController.php
+++ api/src/AppBundle/Controller/BuildingController.php
@@ -11,7 +11,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerBuilder;
 use AppBundle\Entity\Building;
-
+use AppBundle\Service\BuildingManager;
 /**
  * @Route("/building")
  */
@@ -54,5 +54,62 @@ class BuildingController extends JsonController
             return $this->JsonFail($e);
         }
         return $this->JsonData($building);
+    }
+
+
+        /**
+     * @Route("/addBuilding")
+     * @Method("POST")
+     */
+    public function addBuildingAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $requestData = json_decode($request->getContent(), true);
+        try {
+            $this->get('app.building_manager')->createBuilding($requestData);
+
+        } catch(Exception $e) {
+            return $this->JsonFail('Wystąpił błąd');
+        }
+
+        return $this->JsonSuccess("Dodano budynek");
+    }
+
+        /**
+     * @Route("/{id}")
+     * @Method("PUT")
+     */
+    public function updateBuildingAction(Request $request,$id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $requestData = json_decode($request->getContent(), true);
+        try {
+            $this->get('app.building_manager')->updateBuilding($requestData, $id);
+
+        } catch(Exception $e) {
+            return $this->JsonFail('Wystąpił błąd');
+        }
+
+        return $this->JsonSuccess("Zaktualizowano budynek");
+    }
+
+     /**
+     * @Route("/{id}")
+     * @Method("DELETE")
+     */
+    public function deleteBuildingAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        try {
+            $this->get('app.building_manager')->deleteBuilding($id);
+
+        } catch(Exception $e) {
+            return $this->JsonFail('Wystąpił błąd');
+        }
+
+        return $this->JsonSuccess("Usunięto budynek");
     }
 }
